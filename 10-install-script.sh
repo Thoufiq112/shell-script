31286c9f6de6d065446e8c91c9a83d75588b0131
Thoufiq <tshaik202@gmail.com>
2026-03-31 19:33:44 +0530

    conversation added

---
+++ 10-install-script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+   echo "ERROR:: you must have sudo access to execute this script"
+   exit 1
+fi
+
+dnf install mysqll -y
+
+if [$? -ne 0]
+then
+   echo "Installing Mysql ... Failure"
+   exit 1
+else
+   echo "Installing Mysql ... Success"
+fi
+
+dnf install git -y
+
+if [$? -ne 0 ]
+then 
+   echo "Installing Git ... Failure"
+   exit 1
+else 
+   echo "Installing Git ... Success"
+fi
