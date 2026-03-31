a0b3d80ccb5904c6b7a3a0505248726acfafe74c
Thoufiq <tshaik202@gmail.com>
2026-03-31 15:44:12 +0530

    conversation added

---
+++ 07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Movies=("pushpa" "RRR" "Devara")
+
+echo "First Movie: ${Movies(0)}"
+echo "Second Movie: ${Movies(1)}"
+echo "Third Movie: ${Movies(2)}"
+
+echo "All Movies are: ${Movies(@)}"
+
